9a9383e9d15f153151e46702bfacc5eb21b26cbb
root <root@localhost.localdomain>
2020-02-01 16:20:58 +0600

    stable database added

--- conf.py
+++ conf.py
@@ -12,3 +12,13 @@ class auth:
 
 	BOT_TOKEN = 'EAAC5audzX3QBAGSoEo8bU3zvFAkoJMrZBJY0UooxavyiBFJ48ZBFtD6ZBPeEi3ZBc8xr9ZCNNPxznNtRK9DsFlNAUFOdesRfzZCLgOtzgmtypTWGTogUCZCen5zyWU98DJOk8A1EqnMg2sTk0T02TuZCB0KcVToN0NNALhlZChQgpbKYK8XFdKxdL'
 	VERIFY_TOKEN = 'itsabidtheclasscaptain'
+
+	db_info = {
+		'email': 'fotruluydi@enayu.com',
+		'pass': 'wbi',
+		'Username': 'jMQ1b4AojB',
+		'Database': 'jMQ1b4AojB',
+		'Password': 'lpQ506EJPA',
+		'Server': 'remotemysql.com',
+		'Port': 3306
+	}
--- database.py
+++ database.py
@@ -1,21 +1,22 @@
-import sqlite3
+import pymysql
+from conf import auth
 
 class DB:
 
 	def __init__(self):
-		self.conn = sqlite3.connect('db/subscriber.db')
-		# self.conn.execute('''create table if not exists class_room(id text PRIMARY KEY, class_id text, member_type text)''')
+		self.conn = pymysql.connections.Connection(host=auth.db_info['Server'], user=auth.db_info['Username'], password=auth.db_info['Password'], database=auth.db_info['Database'])
+		# self.conn.execute('''create table if not exists class_room(id text PRIMARY KEY, class_id integer, member_type text)''')
 
 
 	def addMember(self, fb_id, classID=None, status='PENDING'):
 		c = self.conn.cursor()
-		c.execute('''INSERT INTO class_room VALUES(?,?,?)''',(fb_id, classID, status))
+		c.execute('''INSERT INTO class_room VALUES(%s,%s,%s)''',(fb_id, classID, status))
 		self.conn.commit()
-	
-	
+
+
 	def checkMember(self, fb_id):
 		c = self.conn.cursor()
-		c.execute('''SELECT member_type, class_id FROM class_room WHERE id=?''',(fb_id,))
+		c.execute('''SELECT member_type, class_id FROM class_room WHERE id=%s''',(fb_id,))
 		val = c.fetchone()
 		if val:
 			return val[0] if val[1] else 'NEW'
@@ -30,32 +31,35 @@ class DB:
 	def getReceiverList(self, specific=None, member='SUBSCRIBER'):
 		c = self.conn.cursor()
 		if specific:
-			q = '''SELECT id FROM class_room WHERE class_id=?'''
+			q = '''SELECT id FROM class_room WHERE class_id=%s'''
 			return [c.execute(q,(i,)).fetchone()[0] for i in specific]
-		c.execute('''SELECT id FROM class_room WHERE member_type=?''',(member,))
+		c.execute('''SELECT id FROM class_room WHERE member_type=%s''',(member,))
 		val = c.fetchall()
 		return [i[0] for i in val] if val else None
 
 
 	def getClassID(self, fb_id):
 		c = self.conn.cursor()
-		c.execute('''SELECT class_id FROM class_room WHERE id=?''',(fb_id,))
+		c.execute('''SELECT class_id FROM class_room WHERE id=%s''',(fb_id,))
 		return c.fetchone()[0]
 
 
 	def update(self, classID, status='SUBSCRIBER'):
 		c = self.conn.cursor()
-		c.execute('''UPDATE class_room SET member_type=? WHERE class_id=?''',(status, classID))
+		c.execute('''UPDATE class_room SET member_type=%s WHERE class_id=%s''',(status, classID))
 		self.conn.commit()
 
 
 	def updateID(self,fb_id, classID):
 		c = self.conn.cursor()
-		c.execute('''UPDATE class_room SET class_id=? WHERE id=?''',(classID, fb_id))
+		c.execute('''UPDATE class_room SET class_id=%s WHERE id=%s''',(classID, fb_id))
 		self.conn.commit()
 
 
 	def remove(self, classID):
 		c = self.conn.cursor()
-		c.execute('''DELETE FROM class_room WHERE class_id=? ''',(classID,))
+		c.execute('''DELETE FROM class_room WHERE class_id=%s ''',(classID,))
 		self.conn.commit()
+
+
+
