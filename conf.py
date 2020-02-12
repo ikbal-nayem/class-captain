2204f2d9f6bb99544a1c7591add8c41014bc46bc
iku <31121811+ikbal-nayem@users.noreply.github.com>
2020-02-13 00:54:14 +0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,11 +1,11 @@
 class auth:
-	BOT_TOKEN = '<your_messenger_bot_token_given_by_facebook>'
-	VERIFY_TOKEN = '<your_app_verify_token>'
+	BOT_TOKEN = 'EAAC5audzX3QBAGSoEo8bU3zvFAkoJMrZBJY0UooxavyiBFJ48ZBFtD6ZBPeEi3ZBc8xr9ZCNNPxznNtRK9DsFlNAUFOdesRfzZCLgOtzgmtypTWGTogUCZCen5zyWU98DJOk8A1EqnMg2sTk0T02TuZCB0KcVToN0NNALhlZChQgpbKYK8XFdKxdL'
+	VERIFY_TOKEN = 'itsabidtheclasscaptain'
 
 	db_info = {
-		'Username': '<database_username>',
-		'Database': '<database_name>',
-		'Password': '<database_password>',
-		'Server': '<database_address>',
+		'Username': 'jMQ1b4AojB',
+		'Database': 'jMQ1b4AojB',
+		'Password': 'lpQ506EJPA',
+		'Server': 'remotemysql',
 		'Port': 3306
 	}
